f2efe245031ad1c27f346a2e39849cca7f954bc9
ThaBeanBoy <gtchipoyera@gmail.com>
2024-02-23 00:19:12 +0200

    Wrote javadoc for the Node class

--- src/main/java/uc/za/uj/tgchipoyera/linkedlist/LinkedLists/Node.java
+++ src/main/java/uc/za/uj/tgchipoyera/linkedlist/LinkedLists/Node.java
@@ -1,27 +1,52 @@
 package uc.za.uj.tgchipoyera.linkedlist.LinkedLists;
 
+/**
+ * Node for a linked list
+ * @param <E> The type to be stored in the Node
+ */
 public class Node<E> {
-    private E element;
+    private final E element;
     private Node<E> next;
 
+    /**
+     * Constructs Node object
+     * @param element the element stored by the node
+     * @param next the next node after this node
+     */
     public Node(E element, Node<E> next){
         this.element = element;
         this.next = next;
     }
 
+    /**
+     * Constructs Node object with the next Node reference set to `null`
+     * @param element the element stored by the node
+     */
     public Node(E element){
         this.element = element;
         this.next = null;
     }
 
+    /**
+     * Gets the element stored in the Node
+     * @return element
+     */
     public E getElement(){
         return this.element;
     }
 
+    /**
+     * Gets the next Node object after this Node object
+     * @return Node
+     */
     public Node<E> getNext(){
         return this.next;
     }
 
+    /**
+     * Sets the next Node object
+     * @param next Node object after this Node object
+     */
     public void setNext(Node<E> next){
         this.next = next;
     }
